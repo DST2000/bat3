c7d4e6481d396e71cb0aca772191c0ffb6585b8b
DST2000 <dnkst1c@rambler.ru>
2018-08-07 13:25:39 +0300

    Commit 11 add tmp_battmpl

---
+++ templates/DevTemplate/battmpl/includes/head.php
@@ -0,0 +1,16 @@
+<?php
+defined('_JEXEC') or die;
+JHtml::_('bootstrap.framework');
+?>
+<head>
+	<jdoc:include type="head" />
+<meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+	<!--[if lte IE 8]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<?php  if ($pie == 1) : ?>
+			<style>
+				{behavior:url(<?php  echo $tpath; ?>/js/PIE.htc);}
+			</style>
+		<?php  endif; ?>
+	<![endif]-->
+</head>
