f24da3541a3809421f4f872eadb3d03dd770a06f
DST2000 <dnkst1c@rambler.ru>
2018-08-08 14:02:35 +0300

    Commit 19 mod_batcategoryfilter display existing value of  custum fields in category

--- Dev/mod_batcategoryfilter/helper.php
+++ Dev/mod_batcategoryfilter/helper.php
@@ -30,96 +30,110 @@ class modBatCategoryFilterHelper extends VmView
 		$categorylist = "";
 		$searchCustomValues = "";
 		$db =JFactory::getDBO();
-		$virtuemart_category_id = $_REQUEST['virtuemart_category_id'];
+		if (!empty($_REQUEST['virtuemart_category_id'])) 
+		{
+			$virtuemart_category_id = $_REQUEST['virtuemart_category_id'];
+		}
+		else
+		{
+			$virtuemart_category_id = null;
+		}
+
 		
-		$q1= 'SELECT c.* FROM #__virtuemart_customs  as c ';
-		if(!empty($virtuemart_category_id)){
-			$q1 .= 'INNER JOIN #__virtuemart_product_customfields as pc on (c.virtuemart_custom_id=pc.virtuemart_custom_id)
-INNER JOIN #__virtuemart_product_categories as cat ON (pc.virtuemart_product_id=cat.virtuemart_product_id)';
-		}
-		$q1 .= ' WHERE';
-		if(!empty($virtuemart_category_id)){
-			$q1 .= ' virtuemart_category_id="'.$virtuemart_category_id.'" and';
-		}
-		$q1 .= ' searchable="1" and (field_type="S" or field_type="P") and c.published = 1 GROUP BY c.virtuemart_custom_id';
-
-		$db->setQuery($q1);
-		$selectedlist = $db->loadObjectList();
-		//vmdebug('getSearchCustom '.str_replace('#__',$db->getPrefix(),$db->getQuery()),$selectedlist);//,$virtuemart_category_id,$selectedlist);
-		if($selectedlist) {
-			$app = JFactory::getApplication();
-			foreach ($selectedlist as $selected) {
-				$valueOptions = array();
-				if($selected->field_type=="S") {
-
-					//if($selected->is_list) {
-						//if($selected->is_list == "1") {
-						$q2= 'SELECT pc.* FROM #__virtuemart_product_customfields  as pc ';
-						$q2 .= 'INNER JOIN #__virtuemart_products as p on (pc.virtuemart_product_id=p.virtuemart_product_id)';
-						if(!empty($virtuemart_category_id)){
-							$q2 .= 'INNER JOIN #__virtuemart_product_categories as cat on (pc.virtuemart_product_id=cat.virtuemart_product_id)';
-						}
-						$q2 .= ' WHERE virtuemart_custom_id="'.$selected->virtuemart_custom_id.'" and p.published="1" ';
-						if(!empty($virtuemart_category_id)){
-							$q2 .= ' and virtuemart_category_id="'.$virtuemart_category_id.'" ';
-						}
-						$q2 .= ' GROUP BY `customfield_value`';
-
-						/*$q2 = 'SELECT * FROM `#__virtuemart_product_customfields` WHERE virtuemart_custom_id="'.$selected->virtuemart_custom_id.'" ';
-						if(!empty($virtuemart_category_id)){
-							$q1 .= ' virtuemart_category_id="'.$virtuemart_category_id.'" and';
-						}
-						$q2 = 'GROUP BY `customfield_value` ';*/
-						$db->setQuery( $q2 );
-						$Opts = $db->loadObjectList();
-						//vmdebug('getSearchCustom my  q2 '.str_replace('#__',$db->getPrefix(),$db->getQuery()) );
-						if($Opts){
-							foreach( $Opts as $k => $v ) {
-								if(!isset($valueOptions[$v->customfield_value])) {
-									$valueOptions[$v->customfield_value] = $v->customfield_value;
-								}
-							}
-							
-							$v = $app->getUserStateFromRequest ('com_virtuemart.customfields.'.$selected->virtuemart_custom_id, 'customfields['.$selected->virtuemart_custom_id.']', '', 'string');
-							$searchCustomValues .= '<div class="vm-search-custom-values-group"><div class="vm-custom-title-select">' .  vmText::_( $selected->custom_title ).'</div>'.JHtml::_( 'select.genericlist', $valueOptions, 'customfields['.$selected->virtuemart_custom_id.']', 'class="inputbox vm-chzn-select changeSendForm"', 'virtuemart_custom_id', 'custom_title', $v ) . '</div>';
-						}
-
-						//vmdebug('getSearchCustom '.$q2,$Opts,$valueOptions);
-						/*} else if($selected->is_list == "2" and !empty($selected->custom_value)) {
-							$valueOptions = array();
-							$Opts = explode( ';', $selected->custom_value );
-							foreach( $Opts as $k => $v ) {
-								$valueOptions[$v] = $v;
-							}
-						}*/
-
-				} else if($selected->field_type=="P"){
-					$v = vRequest::getString('customfields['.$selected->virtuemart_custom_id.']');
-					$n = 'customfields['.$selected->virtuemart_custom_id.']';
-					$searchCustomValues .= vmText::_( $selected->custom_title ).' <input name="'.$n.'" class="inputbox vm-chzn-select" type="text" size="20" value="'.$v.'"/>';
-				} else {
-				//Atm not written for other field types
-				/*	$db->setQuery('SELECT `customfield_value` as virtuemart_custom_id,`custom_value` as custom_title FROM `#__virtuemart_product_customfields` WHERE virtuemart_custom_id='.$selected->virtuemart_custom_id);
-					$valueOptions= $db->loadAssocList();
-
-					$valueOptions = array_merge(array($emptyOption), $valueOptions);
-					$searchCustomValues .= '<div class="vm-search-custom-values-group"><div class="vm-search-title">'. vmText::_($selected->custom_title).'</div> '.JHtml::_('select.genericlist', $valueOptions, 'customfields['.$selected->virtuemart_custom_id.']', 'class="inputbox vm-chzn-select"', 'virtuemart_custom_id', 'custom_title', 0) . '</div>';*/
-
-				}
+		if (isset($virtuemart_category_id)) 
+		{
+		
+		
+			$q1= 'SELECT c.* FROM #__virtuemart_customs  as c ';
+			if(!empty($virtuemart_category_id)){
+				$q1 .= 'INNER JOIN #__virtuemart_product_customfields as pc on (c.virtuemart_custom_id=pc.virtuemart_custom_id)
+	INNER JOIN #__virtuemart_product_categories as cat ON (pc.virtuemart_product_id=cat.virtuemart_product_id)';
 			}
-			$searchCustomValues .= '<div class="clear"></div>';
-		}
+			$q1 .= ' WHERE';
+			if(!empty($virtuemart_category_id)){
+				$q1 .= ' virtuemart_category_id="'.$virtuemart_category_id.'" and';
+			}
+			$q1 .= ' searchable="1" and (field_type="S" or field_type="P") and c.published = 1 GROUP BY c.virtuemart_custom_id';
 
-		if(VmConfig::get('useCustomSearchTrigger',false)){
-			// add search for declared plugins
-			JPluginHelper::importPlugin('vmcustom');
-			$dispatcher = JDispatcher::getInstance();
-			$plgDisplay = $dispatcher->trigger('plgVmSelectSearchableCustom',array( &$this->options,&$searchCustomValues,$this->custom_parent_id ) );
+			$db->setQuery($q1);
+			$selectedlist = $db->loadObjectList();
+			//vmdebug('getSearchCustom '.str_replace('#__',$db->getPrefix(),$db->getQuery()),$selectedlist);//,$virtuemart_category_id,$selectedlist);
+			if($selectedlist) {
+				$app = JFactory::getApplication();
+				foreach ($selectedlist as $selected) {
+					$valueOptions = array();
+					if($selected->field_type=="S") {
+
+						//if($selected->is_list) {
+							//if($selected->is_list == "1") {
+							$q2= 'SELECT pc.* FROM #__virtuemart_product_customfields  as pc ';
+							$q2 .= 'INNER JOIN #__virtuemart_products as p on (pc.virtuemart_product_id=p.virtuemart_product_id)';
+							if(!empty($virtuemart_category_id)){
+								$q2 .= 'INNER JOIN #__virtuemart_product_categories as cat on (pc.virtuemart_product_id=cat.virtuemart_product_id)';
+							}
+							$q2 .= ' WHERE virtuemart_custom_id="'.$selected->virtuemart_custom_id.'" and p.published="1" ';
+							if(!empty($virtuemart_category_id)){
+								$q2 .= ' and virtuemart_category_id="'.$virtuemart_category_id.'" ';
+							}
+							$q2 .= ' GROUP BY `customfield_value`';
+
+							/*$q2 = 'SELECT * FROM `#__virtuemart_product_customfields` WHERE virtuemart_custom_id="'.$selected->virtuemart_custom_id.'" ';
+							if(!empty($virtuemart_category_id)){
+								$q1 .= ' virtuemart_category_id="'.$virtuemart_category_id.'" and';
+							}
+							$q2 = 'GROUP BY `customfield_value` ';*/
+							$db->setQuery( $q2 );
+							$Opts = $db->loadObjectList();
+							//vmdebug('getSearchCustom my  q2 '.str_replace('#__',$db->getPrefix(),$db->getQuery()) );
+							if($Opts){
+								foreach( $Opts as $k => $v ) {
+									if(!isset($valueOptions[$v->customfield_value])) {
+										$valueOptions[$v->customfield_value] = $v->customfield_value;
+									}
+								}
+
+								$v = $app->getUserStateFromRequest ('com_virtuemart.customfields.'.$selected->virtuemart_custom_id, 'customfields['.$selected->virtuemart_custom_id.']', '', 'string');
+								$searchCustomValues .= '<div class="vm-search-custom-values-group"><div class="vm-custom-title-select">' .  vmText::_( $selected->custom_title ).'</div>'.JHtml::_( 'select.genericlist', $valueOptions, 'customfields['.$selected->virtuemart_custom_id.']', 'class="inputbox vm-chzn-select changeSendForm"', 'virtuemart_custom_id', 'custom_title', $v ) . '</div>';
+							}
+
+							//vmdebug('getSearchCustom '.$q2,$Opts,$valueOptions);
+							/*} else if($selected->is_list == "2" and !empty($selected->custom_value)) {
+								$valueOptions = array();
+								$Opts = explode( ';', $selected->custom_value );
+								foreach( $Opts as $k => $v ) {
+									$valueOptions[$v] = $v;
+								}
+							}*/
+
+					} else if($selected->field_type=="P"){
+						$v = vRequest::getString('customfields['.$selected->virtuemart_custom_id.']');
+						$n = 'customfields['.$selected->virtuemart_custom_id.']';
+						$searchCustomValues .= vmText::_( $selected->custom_title ).' <input name="'.$n.'" class="inputbox vm-chzn-select" type="text" size="20" value="'.$v.'"/>';
+					} else {
+					//Atm not written for other field types
+					/*	$db->setQuery('SELECT `customfield_value` as virtuemart_custom_id,`custom_value` as custom_title FROM `#__virtuemart_product_customfields` WHERE virtuemart_custom_id='.$selected->virtuemart_custom_id);
+						$valueOptions= $db->loadAssocList();
+
+						$valueOptions = array_merge(array($emptyOption), $valueOptions);
+						$searchCustomValues .= '<div class="vm-search-custom-values-group"><div class="vm-search-title">'. vmText::_($selected->custom_title).'</div> '.JHtml::_('select.genericlist', $valueOptions, 'customfields['.$selected->virtuemart_custom_id.']', 'class="inputbox vm-chzn-select"', 'virtuemart_custom_id', 'custom_title', 0) . '</div>';*/
+
+					}
+				}
+				$searchCustomValues .= '<div class="clear"></div>';
+			}
+
+			if(VmConfig::get('useCustomSearchTrigger',false)){
+				// add search for declared plugins
+				JPluginHelper::importPlugin('vmcustom');
+				$dispatcher = JDispatcher::getInstance();
+				$plgDisplay = $dispatcher->trigger('plgVmSelectSearchableCustom',array( &$this->options,&$searchCustomValues,$this->custom_parent_id ) );
+			}
+			//vmTime('getSearchCustom after trigger','getSearchCustom');
+			vmJsApi::chosenDropDowns();
+
+			return $searchCustomValues;
+
 		}
-		//vmTime('getSearchCustom after trigger','getSearchCustom');
-		vmJsApi::chosenDropDowns();
-		
-		return $searchCustomValues;
     }
 	
 	/*
--- Dev/mod_batcategoryfilter/tmpl/default.php
+++ Dev/mod_batcategoryfilter/tmpl/default.php
@@ -3,7 +3,14 @@
 if($searchCustomValues) {
 			echo ($searchCustomValues);
 		}
-$virtuemart_category_id = $_REQUEST['virtuemart_category_id'];
+if (!empty($_REQUEST['virtuemart_category_id'])) 
+	{
+		$virtuemart_category_id = $_REQUEST['virtuemart_category_id'];
+	}
+else
+	{
+		$virtuemart_category_id = "category_not_set";
+	}
 echo "<p class='virtuemart_category_id'>".$virtuemart_category_id."</p>";
 
 ?>
