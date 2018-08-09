327eb5a9aa10372b7fd5ebe328da054ab4c63512
DST2000 <dnkst1c@rambler.ru>
2018-08-09 16:27:34 +0300

    Commit 21 mod_batcategoryfilter range slider shown

--- Dev/mod_batcategoryfilter/tmpl/default.php
+++ Dev/mod_batcategoryfilter/tmpl/default.php
@@ -20,3 +20,49 @@ echo "<p class='virtuemart_category_id'>".$virtuemart_category_id."</p>";
 <label for="customRange3">Example range</label>
 <input type="range" class="custom-range" min="0" max="5" step="0.5" id="customRange3">
 
+
+<p> Single-value slider, high track: </p>
+<input id="ex12a" type="text"/><br/>
+Note that there is no low track on the single-value slider. The area to lesser than the value of the handle is the selection.
+
+<p> Range slider, low track: </p>
+<input id="ex12b" type="text"/><br/>
+
+<p> Range slider, low and high tracks, and selection: </p>
+<input id="ex12c" type="text"/><br/>
+
+<br>
+
+<span id="ex18-label-2a" class="hidden">Example low value</span>
+<span id="ex18-label-2b" class="hidden">Example high value</span>
+<input id="ex18b" type="text"/>
+
+<script>
+// With JQuery
+jQuery("#ex12a").slider({ id: "slider12a", min: 0, max: 10, value: 5 });
+jQuery("#ex12b").slider({ id: "slider12b", min: 0, max: 10, range: true, value: [3, 7] });
+jQuery("#ex12c").slider({ id: "slider12c", min: 0, max: 10, range: true, value: [3, 7] });
+	
+	
+jQuery("#ex18b").slider({
+	min: 0,
+	max: 10,
+	value: [3, 6],
+	labelledby: ['ex18-label-2a', 'ex18-label-2b']
+});
+</script>
+
+<style>
+#slider12a .slider-track-high, #slider12c .slider-track-high {
+	background: green;
+}
+
+#slider12b .slider-track-low, #slider12c .slider-track-low {
+	background: red;
+}
+
+#slider12c .slider-selection {
+	background: yellow;
+}
+</style>
+
--- templates/DevTemplate/battmpl/includes/head.php
+++ templates/DevTemplate/battmpl/includes/head.php
@@ -5,7 +5,9 @@ $tpath = $this->baseurl.'/templates/'.$this->template;
 ?>
 <head>
 	<jdoc:include type="head" />
-	<link href="<?php echo $tpath; ?>.'/css/jui/bootstrap.min.css'" rel="stylesheet" type="text/css" />
+	<link href="<?php echo $tpath; ?>/css/jui/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?php echo $tpath; ?>/css/bootstrap-slider.css" rel="stylesheet" type="text/css" />
+	<script src="<?php echo $tpath; ?>/js/bootstrap-slider.js" type="text/javascript"></script>
 <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
 	<!--[if lte IE 8]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
